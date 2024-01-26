8ab865616d4d56e5a591c0613211673e09fbde79
Hariniaina1764! <grace.andrianisoa@gmail.com>
2024-01-26 10:06:10 +0100

    fix move DemoApplication to root

--- src/main/java/com/demo/vehicule/ServletInitializer.java
+++ src/main/java/com/demo/vehicule/ServletInitializer.java
@@ -3,6 +3,8 @@ package com.demo.vehicule;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.demo.DemoApplication;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
